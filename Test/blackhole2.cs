fd1976675f2cec30762802c2917b6c35d9887eda
senshirou <senshirou@hotmail.com>
2019-05-01 21:10:11 +0900

    Test

---
+++ Test/blackhole2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blackhole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject,2f);
+        }
+    }
+}
